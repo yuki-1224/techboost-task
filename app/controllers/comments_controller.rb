2d808b2aeddb436cba52a574687ce67d7979e095
take <youxizhn4116@gmail.com>
2020-09-06 05:15:33 +0000

    comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   def new
     @topic = Topic.find_by(id: params[:topic_id]) 
-    @comment =Comment.new
+    @comment = Comment.new
+    @comments = Comment.where(topic_id: params[:topic_id])
   end
   
   def create
@@ -9,10 +10,15 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     if @comment.save
       # flashはなくてもいいかも
-      redirect_to topics_path, success: "コメントを送信しました"
+      redirect_to comments_new_path, success: "コメントを送信しました"
     else
+      @topic = Topic.find_by(id: params[:topic_id])
       flash.now[:danger] = "コメントを送れないのよ"
       render :new
     end
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:sentence, :topic_id)
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,7 +6,7 @@ class Topic < ApplicationRecord
   belongs_to :user
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
-  has_many :comment
+  has_many :comments
   
   mount_uploader :image, ImageUploader
 end
